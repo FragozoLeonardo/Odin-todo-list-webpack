ccdd4184601885555a695b46eeb589ba0f0c2a10
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2024-04-03 18:45:37 -0300

    Project are now saving

--- src/project.js
+++ src/project.js
@@ -1,18 +1,7 @@
 export class Project {
   constructor(name) {
-    this.id = Math.random().toString(36).substr(2, 9); // Generating unique ID
+    this.id = Date.now().toString();
     this.name = name;
     this.todos = [];
-  }
-
-  addTodo(todo) {
-    this.todos.push(todo);
-  }
-
-  removeTodo(todo) {
-    const index = this.todos.indexOf(todo);
-    if (index !== -1) {
-      this.todos.splice(index, 1);
-    }
   }
 }
--- src/ui.js
+++ src/ui.js
@@ -3,9 +3,22 @@ import { Project } from './project';
 import { Todo } from './todo';
 import { saveProjectsToLocalStorage, loadProjectsFromLocalStorage } from './storage';
 
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+    modal.style.display = 'none';
+  }
+}
+
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  if (modal) {
+    modal.style.display = 'block';
+  }
+}
+
 export function initializeApp() {
   let projects = loadProjectsFromLocalStorage();
-
   renderProjects(projects);
 
   const addProjectForm = document.getElementById('project-form');
@@ -19,10 +32,27 @@ export function initializeApp() {
     closeModal('modal');
   });
 
-  const projectsContainer = document.getElementById('projects');
-  projectsContainer.addEventListener('change', function(event) {
-    const projectId = event.target.value;
-    const selectedProject = projects.find(project => project.id === projectId);
+  const addTodoForm = document.getElementById('todo-form');
+  addTodoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const todoTitle = document.getElementById('todo-title').value;
+    const todoDescription = document.querySelector('.todo-description').value;
+    const todoDueDate = document.querySelector('.todo-due-date').value;
+    const todoPriority = document.querySelector('.todo-priority').value;
+    const todoNotes = document.querySelector('.todo-notes').value;
+    const selectedProjectId = document.getElementById('project-select').value;
+    const selectedProject = projects.find(project => project.id === selectedProjectId);
+    
+    if (!selectedProject) {
+      alert("Please select a project to add this todo.");
+      return;
+    }
+
+    const todo = new Todo(todoTitle, todoDescription, todoDueDate, todoPriority, todoNotes);
+    selectedProject.todos = selectedProject.todos || [];
+    selectedProject.todos.push(todo);
+    saveProjectsToLocalStorage(projects);
     renderTodos(selectedProject.todos);
+    closeModal('todo-modal');
   });
 }
